7f9a18a776c49e8440e4fa9defbc4f205281bfab
Shaun Dowling <shaun.dowling@ef.uk.com>
2024-02-14 11:30:59 +0000

    move data and copy in build

--- src/app/api/query/route.ts
+++ src/app/api/query/route.ts
@@ -1,6 +1,6 @@
 import EnergyDataServer from "@/app/services/EnergyDataServer";
 
-const dataServer = new EnergyDataServer(`${process.cwd()}/src/data`);
+const dataServer = new EnergyDataServer(`${process.cwd()}/data`);
 
 export async function POST(request: Request) {
   const body = await request.json();
